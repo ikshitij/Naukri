b068d8e1f9ba14a0b40dcebdc61887d93f993d20
ikshitij <48786591+ikshitij@users.noreply.github.com>
2025-01-19 00:49:14 +0530

    Update Login.java

--- src/main/java/Automation/Naukri/Login.java
+++ src/main/java/Automation/Naukri/Login.java
@@ -4,14 +4,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Login {
     public static void main(String[] args) throws InterruptedException {
         // Set the path to the ChromeDriver executable
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
-   
- WebDriver driver = new ChromeDriver();
+ChromeOptions options = new ChromeOptions();
+options.addArguments("--headless");
+options.addArguments("--no-sandbox");
+options.addArguments("--disable-dev-shm-usage");
+options.addArguments("--remote-debugging-port=9222");  // Optional but sometimes required for stability
+
+WebDriver driver = new ChromeDriver(options);
         
             // Navigate to the login page
             driver.get("https://www.naukri.com/nlogin/login?utm_source=google&utm_medium=cpc&utm_campaign=Brand_Login_Register&gad_source=1&gclid=Cj0KCQiAv628BhC2ARIsAIJIiK-gbJ6OBjtci_xwXy7GkpCnIuy8FYIpVJD55_0EJ2jiD8dXO0fQwP8aAoSHEALw_wcB&gclsrc=aw.ds");
