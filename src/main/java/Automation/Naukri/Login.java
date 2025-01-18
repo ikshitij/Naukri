c00c3edf6d48f3423be1a412339df510126b674c
ikshitij <48786591+ikshitij@users.noreply.github.com>
2025-01-19 00:22:15 +0530

    chromedriver path updated

--- src/main/java/Automation/Naukri/Login.java
+++ src/main/java/Automation/Naukri/Login.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Login {
     public static void main(String[] args) throws InterruptedException {
         // Set the path to the ChromeDriver executable
-        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
         // Initialize the WebDriver (Chrome browser)
         WebDriver driver = new ChromeDriver();
