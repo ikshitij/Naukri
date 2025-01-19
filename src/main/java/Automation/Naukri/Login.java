b67056b63a648835af845a2bb38e5651a9331d65
ikshitij <48786591+ikshitij@users.noreply.github.com>
2025-01-19 11:14:04 +0530

    webdriver wait added

--- src/main/java/Automation/Naukri/Login.java
+++ src/main/java/Automation/Naukri/Login.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
 
 public class Login {
     public static void main(String[] args) throws InterruptedException {
@@ -22,8 +26,8 @@ WebDriver driver = new ChromeDriver(options);
             // Navigate to the login page
             driver.get("https://www.naukri.com/nlogin/login?utm_source=google&utm_medium=cpc&utm_campaign=Brand_Login_Register&gad_source=1&gclid=Cj0KCQiAv628BhC2ARIsAIJIiK-gbJ6OBjtci_xwXy7GkpCnIuy8FYIpVJD55_0EJ2jiD8dXO0fQwP8aAoSHEALw_wcB&gclsrc=aw.ds");
 
-            // Locate the username field and enter the username
-            WebElement usernameField = driver.findElement(By.id("usernameField")); // Replace "username" with the actual ID of the username field
+                        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement usernameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("usernameField")));
             usernameField.sendKeys("kshitijsalunkhe@gmail.com");
 
             // Locate the password field and enter the password
